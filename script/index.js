5126de8fc72cf371b92e1978c1278fc612ff0383
GAPP2434 <122450325+GAPP2434@users.noreply.github.com>
2024-04-20 21:30:36 +0800

    Fixes

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+window.addEventListener('scroll', function() {
+  var headerContainer = document.querySelector('.header-container');
+  if (window.scrollY > 0) {
+      headerContainer.classList.add('scroll');
+  } else {
+      headerContainer.classList.remove('scroll');
+  }
+});
 window.onload = function() {
     plusSlides(0);
   };
