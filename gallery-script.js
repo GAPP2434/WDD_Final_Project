91a7d165f051dc7c104045089104f940828f87ba
GAPP2434 <122450325+GAPP2434@users.noreply.github.com>
2024-04-21 14:31:59 +0800

    added modal to homepage

--- gallery-script.js
+++ gallery-script.js
@@ -39,6 +39,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+/*modal*/
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
