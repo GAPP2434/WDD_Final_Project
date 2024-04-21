40e9e6f63ee1849650dd9e213a6fe448e6ef0a0b
GAPP2434 <122450325+GAPP2434@users.noreply.github.com>
2024-04-21 15:34:56 +0800

    donaet

---
+++ donate.php
@@ -0,0 +1,9 @@
+<?php
+    include 'dbcon.php';
+    date_default_timezone_set('Asia/Manila');
+    $currentdate = date('Y-m-d H:i:s');
+    $amount = $_POST['amount'];
+
+    $signup = mysqli_query($conn, "INSERT INTO `tbldonation`(`dAmount`, `dDate`) 
+    VALUES ('".$amount."','".$currentdate."')");
+?>
